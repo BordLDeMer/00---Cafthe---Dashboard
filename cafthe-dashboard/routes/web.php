e7a5181a9d2576973c233fbbf98d0c7bb8de54a0
benjamin.bidou <benjamin.bidou@campus-centre.fr>
2025-07-16 13:14:29 +0200

    + DashBoard

--- cafthe-dashboard/routes/web.php
+++ cafthe-dashboard/routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
     Route::get('/', function () {return view('welcome');});
     Route::get('/sales', [SaleController::class, 'index']);
     Route::get('/inventory', [InventoryController::class, 'index']);
     Route::get('/employees', [EmployeeController::class, 'index']);
-
-
+    Route::get('/dashboard', [DashboardController::class, 'index']);
