d42978f91089a312060ba2479e3dcfab3d6bdf57
benjamin.bidou <benjamin.bidou@campus-centre.fr>
2025-08-13 14:12:31 +0200

    Merci Sacha

--- cafthe-dashboard/routes/web.php
+++ cafthe-dashboard/routes/web.php
@@ -3,8 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 
-    Route::get('/', function () {return view('welcome');});
     Route::get('/sales', [SaleController::class, 'index']);
     Route::get('/inventory', [InventoryController::class, 'index']);
     Route::get('/employees', [EmployeeController::class, 'index']);
-    Route::get('/dashboard', [DashboardController::class, 'index']);
+    Route::get('/', [DashboardController::class, 'index']);
