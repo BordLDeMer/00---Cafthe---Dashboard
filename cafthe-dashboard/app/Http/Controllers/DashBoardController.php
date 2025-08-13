ea6a9c611359178af47e89bb88520485e5633729
benjamin.bidou <benjamin.bidou@campus-centre.fr>
2025-08-13 15:03:46 +0200

    Merci Sacha

--- cafthe-dashboard/app/Http/Controllers/DashBoardController.php
+++ cafthe-dashboard/app/Http/Controllers/DashBoardController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Sale;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard');
+        // Calcule le chiffre d'affaires du mois en cours
+        $debutMois = now()->startOfMonth()->toDateString();
+        $finMois = now()->endOfMonth()->toDateString();
+        $chiffreAffairesMois = Vente::whereBetween('created_at', [$debutMois, $finMois])
+            ->sum('total_sales_last_month');
+
+        // Passe la variable à la vue
+        return view('dashboard')
+
     }
 }
