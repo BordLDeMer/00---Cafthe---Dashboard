bb8e7eeb32a96f9e9c1d5ce060d8434d46ffadb6
benjamin.bidou <benjamin.bidou@campus-centre.fr>
2025-08-13 14:49:27 +0200

    Merci Sacha

--- cafthe-dashboard/app/Http/Controllers/SaleController.php
+++ cafthe-dashboard/app/Http/Controllers/SaleController.php
@@ -3,22 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Vente; // Remplace par ton modèle réel (par exemple, Commande, Transaction, etc.)
+use App\Models\Sale;
 
 class VenteController extends Controller
 {
-    public function chiffreAffairesMois()
-    {
-        // Récupère le premier et le dernier jour du mois en cours
-        $debutMois = now()->startOfMonth()->toDateString();
-        $finMois = now()->endOfMonth()->toDateString();
-
-        // Récupère la somme des ventes pour le mois en cours
-        $total = Vente::whereBetween('created_at', [$debutMois, $finMois])
-            ->sum('montant'); // Remplace 'montant' par le champ qui contient le montant de la vente
-
-        return response()->json([
-            'total' => $total,
-        ]);
-    }
+    //
 }
